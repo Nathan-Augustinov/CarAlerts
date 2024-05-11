5735706750fefc45d5b150f687f58deaf96cb927
Nathan Augustinov <nathan.augustinov@student.upt.ro>
2024-05-11 13:08:23 +0300

    Chnaged a bit the homepage layout

--- lib/screens/home_page_screen.dart
+++ lib/screens/home_page_screen.dart
@@ -12,16 +12,26 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   final String currentUserId = FirebaseAuth.instance.currentUser!.uid;
+  final String currentUserName = FirebaseAuth.instance.currentUser!.displayName!;
   bool isLoading = true;
   List<Car> myCarList = [];
   List<MapEntry<String, int>> urgentItems = [];
+  late TabController _tabController;
 
   @override
   void initState() {
     super.initState();
     _fetchCars();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
   }
 
   void _signOut(BuildContext context) async {
@@ -35,84 +45,74 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _fetchCars() async {
-    QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
-        .collection('cars')
-        .doc(currentUserId)
-        .collection('user_cars')
-        .get();
-    List<Car> fetchedCars = querySnapshot.docs.map((doc) => Car.fromMap(doc.data(), doc.id)).toList();
     setState(() {
-      myCarList = fetchedCars;
-      urgentItems = myCarList.expand((car) => car.getUrgentItems()).toList()..sort((a, b) => a.value.compareTo(b.value));
-      isLoading = false;
+      isLoading = true; // Set to true when starting to fetch data
     });
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await FirebaseFirestore.instance
+              .collection('cars')
+              .doc(currentUserId)
+              .collection('user_cars')
+              .get();
+      List<Car> fetchedCars = querySnapshot.docs
+          .map((doc) => Car.fromMap(doc.data(), doc.id))
+          .toList();
+      setState(() {
+        myCarList = fetchedCars;
+        urgentItems = myCarList.expand((car) => car.getUrgentItems()).toList()
+          ..sort((a, b) => a.value.compareTo(b.value));
+        isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        isLoading = false;
+      });
+      print("Error fetching cars: $e");
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    List<MapEntry<String, int>> expiredItems = urgentItems.where((item) => item.value < 0).toList();
-    List<MapEntry<String, int>> soonToExpireItems = urgentItems.where((item) => item.value >= 0).toList();
+    List<MapEntry<String, int>> expiredItems =
+        urgentItems.where((item) => item.value < 0).toList();
+    List<MapEntry<String, int>> soonToExpireItems =
+        urgentItems.where((item) => item.value >= 0).toList();
     return Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.blueGrey,
-          title: const Text("Home Page"),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.exit_to_app),
-              onPressed: () => _signOut(context),
-            ),
-          ],
+          title: Text("Hello, $currentUserName!"),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: "Already expired"),
+              Tab(text: "Soon to expire"),
+            ],
+          ),
         ),
         body: isLoading
             ? const Center(child: CircularProgressIndicator())
-            : Column(
-                children: [
-                  if(expiredItems.isNotEmpty) ...[
-                   Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Already expired",
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ))),
-                  Expanded(
-                    flex: 1,
-                    child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: expiredItems.length,
-                        itemBuilder: (context, index) {
-                          var item = expiredItems[index];
-                          return ListTile(
-                            title: Text(item.key,style: const TextStyle(color: Colors.red)),
-                            subtitle: Text("Expired ${item.value.abs()} days ago"),
-                          );
-                        }),
-                  )],
-                  if(soonToExpireItems.isNotEmpty) ...[
-                  Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            "Soon to expire",
-                            style: Theme.of(context).textTheme.titleLarge,
-                          ))),
-                  Expanded(
-                    flex: 1,
-                    child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: soonToExpireItems.length,
-                        itemBuilder: (context, index) {
-                          var item = soonToExpireItems[index];
-                          Color textColor = item.value > 30 ? Colors.green : item.value > 15 ? Colors.orange : Colors.red;
-                          return ListTile(
-                            title: Text(item.key,style: TextStyle(color: textColor)),
-                            subtitle: item.value > 30 ? const Text("Expiring in over 30 days") : Text("Expiring in ${item.value} days"),
-                          );
-                        }),
-                  )]
-                ],
-              ));
+            : TabBarView(controller: _tabController, children: [
+                _buildItemList(expiredItems, "Already expired"),
+                _buildItemList(soonToExpireItems, "Soon to expire")
+              ]));
+  }
+
+  Widget _buildItemList(List<MapEntry<String, int>> items, String category) {
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          var item = items[index];
+          Color textColor = item.value > 30
+              ? Colors.green
+              : item.value > 15
+                  ? Colors.orange
+                  : Colors.red;
+          return ListTile(
+            title: Text(item.key, style: TextStyle(color: textColor)),
+            subtitle: Text(
+                "$category ${item.value > 0 ? "in" : ""} ${item.value.abs()} days ${item.value > 0 ? "" : "ago"}",),
+          );
+        });
   }
 }
--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:car_alerts/constants/app_colors.dart';
 import 'package:car_alerts/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -11,19 +12,53 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // body: Center(
+      //   child: ElevatedButton(
+      //     onPressed: () async {
+      //       User? user = await _authService.signInWithGoogle();
+      //       if (user != null && context.mounted) {
+      //         // Navigate to Home Screen
+      //         Navigator.pushReplacement(
+      //           context,
+      //           MaterialPageRoute(builder: (context) =>  MainScreen())
+      //         );
+      //       }
+      //     },
+      //     child: const Text('Sign in with Google'),
+      //   ),
+      // ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            User? user = await _authService.signInWithGoogle();
-            if (user != null && context.mounted) {
-              // Navigate to Home Screen
-              Navigator.pushReplacement(
-                context, 
-                MaterialPageRoute(builder: (context) =>  MainScreen())
-              );
-            }
-          },
-          child: const Text('Sign in with Google'),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height * 0.07,
+          child: Center(
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 18),
+                minimumSize:
+                    Size.fromWidth(MediaQuery.of(context).size.width * 0.4),
+                foregroundColor: AppColors.firstColor,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(30),
+                  ),
+                ),
+              ),
+              icon: Image.asset(
+                'assets/images/google_sign_in_logo.png',
+                width: 24,
+                height: 24,
+              ),
+              label: const Text('Sign in with Google'),
+              onPressed: () async {
+                User? user = await _authService.signInWithGoogle();
+                if (user != null && context.mounted) {
+                  // Navigate to Home Screen
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => MainScreen()));
+                }
+              },
+            ),
+          ),
         ),
       ),
     );
